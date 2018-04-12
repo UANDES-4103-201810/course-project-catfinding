dc2d7b52ca47a7e5afad50855742bf80bc2e9cd4
Espe <eruribe@miuandes.cl>
2018-04-12 15:13:47 -0300

    commit for merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   has_many :userpasswords
 
   validates :username, length: {minimum: 4}
-  validates :description, length: {minimum: 20}
   validates :mail, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
 end
