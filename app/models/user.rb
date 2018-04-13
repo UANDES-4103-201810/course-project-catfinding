3adffbd3d1c92f4a344ce73ae3cc01112b6c1fe0
Espe <eruribe@miuandes.cl>
2018-04-13 00:54:11 -0300

    fixed age bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ApplicationRecord
   validates :address, length: {minimum: 5}
 
   def validate_age
-    if birthday.present? && (birthday-DateTime.now.to_date > 14)
+    if birthday > 14.years.ago.to_date
       errors.add(:birthday, 'You should be over 14 years old.')
     end
   end
   after_create do
     puts "You have created an user"
   end
+  before_save :validate_age
 end
