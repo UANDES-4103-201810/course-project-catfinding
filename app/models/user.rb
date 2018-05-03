6341c7c7c6eb5ccf2f3689b30ca6f0c5162781d8
Esperanza <eruribe@miuandes.cl>
2018-05-03 10:52:15 -0300

    config the secret key of devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,6 @@
 class User < ApplicationRecord
-  has_many :userpasswords
-  validates :name, length: {minimum: 2}
-  validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
-  validate :validate_age
-  validates :address, length: {minimum: 5}
-
-  def validate_age
-    if birthday > 14.years.ago.to_date
-      errors.add(:birthday, 'You should be over 14 years old.')
-    end
-  end
-  after_create do
-    puts "You have created an user"
-  end
-  before_save :validate_age
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
