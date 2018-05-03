6ff30791a04f3af4629e365ff82944b49113ac86
Esperanza <eruribe@miuandes.cl>
2018-05-03 10:55:29 -0300

    forgot to add the config file foir devise :P

--- app/models/userpassword.rb
+++
@@ -1,7 +0,0 @@
-class Userpassword < ApplicationRecord
-  belongs_to :user
-  validates :password, length: {minimum: 8}
-  after_update do
-    puts "You have changed your password."
-  end
-end
--- test/models/userpassword_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserpasswordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
