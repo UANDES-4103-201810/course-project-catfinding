056de171612e07bddf1aeb972bb7a0e2466e6c74
Esperanza Uribe <esperanza@MacBook-Pro-de-Esperanza.local>
2018-05-25 13:08:22 -0400

    idk what changed this time

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # README 
 This is Esperanza Uribe & Lucas Latuz work
 We created an admin user in the seeds file for debugging purposes, you can use it if
-you want, the credentials are email: fjborie@miuandes.cl password: lalala
-the user with the id 1 has the project 1 and project 2.
+you want, the credentials are 
+email: fjborie@miuandes.cl 	password: lalala for the admin user 
+email: mrgarcia@miuandes.cl 	password: 123456 for the non admin user
+the user with the id 1 has every project
 
 
 Bugs(?):
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_25_031731) do
+ActiveRecord::Schema.define(version: 2018_05_25_160654) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -61,14 +61,6 @@ ActiveRecord::Schema.define(version: 2018_05_25_031731) do
     t.index ["user_id"], name: "index_user_projects_on_user_id"
   end
 
-  create_table "user_roles", force: :cascade do |t|
-    t.boolean "is_admin", default: false, null: false
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_user_roles_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -89,6 +81,7 @@ ActiveRecord::Schema.define(version: 2018_05_25_031731) do
     t.text "description", default: "", null: false
     t.date "birthday", default: "1994-03-16", null: false
     t.string "address"
+    t.boolean "is_admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
