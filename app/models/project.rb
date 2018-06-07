537c3a970028e7721258b4ee4e986b4e44097f66
Esperanza Uribe <esperanza@MacBook-Pro-de-Esperanza.local>
2018-06-07 09:29:01 -0400

    Projet and promises imgs put on

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,6 +8,9 @@ class Project < ApplicationRecord
   has_many :wishlist_projects
   has_many :wishlists, through: :wishlist_projects
 
+  has_attached_file :avatar, styles: { medium: "252x313>", thumb: "100x100>" }, default_url: "/assets/gato_user.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 
   validates :currentamount, numericality: { greater_than_or_equal_to: 0 }
   validates :goalamount, numericality: { greater_than: 0 }
--- app/models/promise.rb
+++ app/models/promise.rb
@@ -1,5 +1,9 @@
 class Promise < ApplicationRecord
   belongs_to :project
+
+  has_attached_file :avatar, styles: { medium: "252x313>", thumb: "100x100>" }, default_url: "/assets/:medium/gato_user.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   validates :name, length: {minimum: 3}
 
   validates :description, length: {minimum: 20}
