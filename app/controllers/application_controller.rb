1da11ec4b3ee132de8b64f5828e9cc48474c0a4b
LucasLatuz <lilatuz@miuandes.cl>
2018-06-12 13:24:08 -0400

    After sign up with facebook go to edit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
-
+  def after_sign_up_path_for
+    redirect_to edit_user_path(@user)
+  end
 end
 
--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,6 +2,10 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in @user
-    redirect_to edit_user_path(@user)
-end
-end
+    if @user.phone.nil? or @user.phone.empty?
+      redirect_to edit_user_path (@user)
+    else
+      redirect_to @user
+  end
+  end
+  end
