79a13d81eb1ab6a6c51e702a9ca78bb7c291e84e
Esperanza Uribe <esperanza@MacBook-Pro-de-Esperanza.local>
2018-06-15 13:45:04 -0400

    laldljbdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "course-project-catfinding_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
